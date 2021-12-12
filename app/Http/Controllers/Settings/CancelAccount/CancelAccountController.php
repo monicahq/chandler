6d3c7987f14e553c3d4b9ff361bfb0e0e760caca
Regis Freyd <djaiss@users.noreply.github.com>
2021-12-12 01:10:12 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/Settings/CancelAccount/CancelAccountController.php
+++ app/Http/Controllers/Settings/CancelAccount/CancelAccountController.php
@@ -5,12 +5,10 @@ namespace App\Http\Controllers\Settings\CancelAccount;
 use Inertia\Inertia;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\Settings\CancelAccount\ViewHelpers\CancelAccountIndexViewHelper;
 use Illuminate\Support\Facades\Auth;
 use App\Services\Account\ManageUsers\InviteUser;
 use App\Http\Controllers\Vault\ViewHelpers\VaultIndexViewHelper;
-use App\Http\Controllers\Settings\Users\ViewHelpers\UserIndexViewHelper;
-use App\Http\Controllers\Settings\Users\ViewHelpers\UserCreateViewHelper;
+use App\Http\Controllers\Settings\CancelAccount\ViewHelpers\CancelAccountIndexViewHelper;
 
 class CancelAccountController extends Controller
 {
