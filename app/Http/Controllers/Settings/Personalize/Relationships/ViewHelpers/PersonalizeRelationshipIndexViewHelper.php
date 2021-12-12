01908101b0bd71e6ae96875e4682a8cbcfc11294
Regis Freyd <djaiss@users.noreply.github.com>
2021-12-12 18:30:31 -0500

    Update PersonalizeRelationshipIndexViewHelper.php

--- app/Http/Controllers/Settings/Personalize/Relationships/ViewHelpers/PersonalizeRelationshipIndexViewHelper.php
+++ app/Http/Controllers/Settings/Personalize/Relationships/ViewHelpers/PersonalizeRelationshipIndexViewHelper.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers\Settings\Personalize\Relationships\ViewHelpers;
 
+use App\Models\Account;
+
 class PersonalizeRelationshipIndexViewHelper
 {
-    public static function data(): array
+    public static function data(Account $account): array
     {
+        $relationshipGroupTypes = $account->groupTypes()
+            ->with('types')
+            ->
         return [
             'url' => [
                 'settings' => route('settings.index'),
