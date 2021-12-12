6f120d1e509c47e39a99212c6da172835c84c749
Regis Freyd <djaiss@users.noreply.github.com>
2021-12-12 01:22:14 +0000

    Apply fixes from StyleCI

--- app/Http/Middleware/CheckAdministratorPrivilege.php
+++ app/Http/Middleware/CheckAdministratorPrivilege.php
@@ -4,7 +4,6 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 
 class CheckAdministratorPrivilege
