0ff5ceeda94fc7d25d557155393c8107f0199a5c
Regis Freyd <djaiss@users.noreply.github.com>
2022-01-15 16:08:34 -0500

    Update 2014_10_12_000000_create_users_table.php

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -37,7 +37,7 @@ class CreateUsersTable extends Migration
             $table->string('type');
             $table->string('name');
             $table->string('description')->nullable();
-            $table->integer('default_template_id')->nullable();
+            $table->unsignedBigInteger('default_template_id')->nullable();
             $table->timestamps();
             $table->foreign('account_id')->references('id')->on('accounts')->onDelete('cascade');
             $table->foreign('default_template_id')->references('id')->on('templates')->onDelete('set null');
