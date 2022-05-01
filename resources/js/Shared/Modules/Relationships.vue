9043c61ab057cc24d87e8aeae963a9f11a205e06
Regis Freyd <djaiss@users.noreply.github.com>
2022-05-01 16:16:42 -0400

    Update Relationships.vue

--- resources/js/Shared/Modules/Relationships.vue
+++ resources/js/Shared/Modules/Relationships.vue
@@ -15,8 +15,17 @@
     <div class="mb-3 items-center justify-between border-b border-gray-200 pb-2 sm:flex">
       <div class="mb-2 sm:mb-0">
         <span class="relative mr-1">
-          <svg xmlns="http://www.w3.org/2000/svg" class="icon-sidebar relative inline h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="icon-sidebar relative inline h-4 w-4"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z" />
           </svg>
         </span>
 
@@ -29,10 +38,7 @@
     <div>
       <h3>Family</h3>
       <div class="mb-4 rounded border border-gray-200 last:mb-0">
-
-        <div>
-
-        </div>
+        <div></div>
       </div>
     </div>
 
