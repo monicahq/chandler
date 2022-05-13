2a615d183e9f9726d87ee54c02cdfe700fe0493f
Regis Freyd <djaiss@users.noreply.github.com>
2022-05-13 14:22:57 -0400

    wp

--- tests/Unit/Models/ContactTest.php
+++ tests/Unit/Models/ContactTest.php
@@ -18,6 +18,9 @@ use App\Models\ContactReminder;
 use App\Models\RelationshipType;
 use App\Models\ContactInformation;
 use App\Models\ContactImportantDate;
+use App\Models\ContactImportantDateType;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class ContactTest extends TestCase
@@ -194,5 +197,77 @@ class ContactTest extends TestCase
         ]);
 
         $this->assertTrue($ross->avatars()->exists());
+    }
+
+    /** @test */
+    public function it_gets_the_name(): void
+    {
+        $user = User::factory()->create([
+            'name_order' => '%first_name%',
+        ]);
+        $contact = Contact::factory()->create([
+            'first_name' => 'James',
+            'last_name' => 'Bond',
+            'nickname' => '007',
+            'middle_name' => 'W.',
+            'maiden_name' => 'Muller',
+        ]);
+
+        $this->assertEquals(
+            'James',
+            $contact->getName($user)
+        );
+
+        $user->update(['name_order' => '%last_name%']);
+        $this->assertEquals(
+            'Bond',
+            $contact->getName($user)
+        );
+
+        $user->update(['name_order' => '%first_name% %last_name%']);
+        $this->assertEquals(
+            'James Bond',
+            $contact->getName($user)
+        );
+
+        $user->update(['name_order' => '%first_name% (%maiden_name%) %last_name%']);
+        $this->assertEquals(
+            'James (Muller) Bond',
+            $contact->getName($user)
+        );
+
+        $user->update(['name_order' => '%last_name% (%maiden_name%)  || (%nickname%) || %first_name%']);
+        $this->assertEquals(
+            'Bond (Muller)  || (007) || James',
+            $contact->getName($user)
+        );
+    }
+
+    /** @test */
+    public function it_gets_the_age(): void
+    {
+        Carbon::setTestNow(Carbon::create(2022, 1, 1));
+        $contact = Contact::factory()->create();
+        $type = ContactImportantDateType::factory()->create([
+            'vault_id' => $contact->vault_id,
+            'internal_type' => ContactImportantDate::TYPE_BIRTHDATE,
+        ]);
+
+        $this->assertNull(
+            $contact->getAge()
+        );
+
+        ContactImportantDate::factory()->create([
+            'contact_id' => $contact->id,
+            'contact_important_date_type_id' => $type->id,
+            'day' => 1,
+            'month' => 10,
+            'year' => 2000,
+        ]);
+
+        $this->assertEquals(
+            21,
+            $contact->getAge()
+        );
     }
 }
