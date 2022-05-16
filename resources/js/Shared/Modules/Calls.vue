02f220463639a22711fd4efaf8dec488fbddd71d
Regis Freyd <djaiss@users.noreply.github.com>
2022-05-16 14:36:55 -0400

    Update Calls.vue

--- resources/js/Shared/Modules/Calls.vue
+++ resources/js/Shared/Modules/Calls.vue
@@ -27,8 +27,17 @@
     <div class="mb-3 items-center justify-between border-b border-gray-200 pb-2 sm:flex">
       <div class="mb-2 sm:mb-0">
         <span class="relative mr-1">
-          <svg xmlns="http://www.w3.org/2000/svg" class="icon-sidebar relative inline h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            class="icon-sidebar relative inline h-4 w-4"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
           </svg>
         </span>
 
@@ -38,17 +47,14 @@
     </div>
 
     <!-- add a task modal -->
-    <form
-      v-if="createCallModalShown"
-      class="bg-form mb-6 rounded-lg border border-gray-200"
-      @submit.prevent="submit()">
+    <form v-if="createCallModalShown" class="bg-form mb-6 rounded-lg border border-gray-200" @submit.prevent="submit()">
       <div class="border-b border-gray-200">
         <div v-if="form.errors.length > 0" class="p-5">
           <errors :errors="form.errors" />
         </div>
 
         <!-- name -->
-        <div class="border-b border-gray-200 flex">
+        <div class="flex border-b border-gray-200">
           <div class="p-5">
             <p class="mb-2 block text-sm">When did the call happened?</p>
             <v-date-picker class="inline-block h-full" v-model="form.date" :model-config="modelConfig">
@@ -62,7 +68,7 @@
             <p class="mb-2 block text-sm">Who called?</p>
 
             <div class="flex">
-              <div class="flex items-center mr-6">
+              <div class="mr-6 flex items-center">
                 <input
                   id="one_time"
                   v-model="form.reminderChoice"
@@ -91,33 +97,33 @@
           </div>
         </div>
 
-        <div class="border-b border-gray-200 p-5 flex">
-            <div class="flex items-center mr-6">
-              <input
-                id="one_time"
-                v-model="form.reminderChoice"
-                value="one_time"
-                name="reminder-frequency"
-                type="radio"
-                class="h-4 w-4 border-gray-300 text-sky-500" />
-              <label for="one_time" class="ml-2 block cursor-pointer text-sm font-medium text-gray-700">
-                This was an audio-only call
-              </label>
-            </div>
-
-            <div class="flex items-center">
-              <input
-                id="one_time"
-                v-model="form.reminderChoice"
-                value="one_time"
-                name="reminder-frequency"
-                type="radio"
-                class="h-4 w-4 border-gray-300 text-sky-500" />
-              <label for="one_time" class="ml-2 block cursor-pointer text-sm font-medium text-gray-700">
-                This was a video call
-              </label>
-            </div>
+        <div class="flex border-b border-gray-200 p-5">
+          <div class="mr-6 flex items-center">
+            <input
+              id="one_time"
+              v-model="form.reminderChoice"
+              value="one_time"
+              name="reminder-frequency"
+              type="radio"
+              class="h-4 w-4 border-gray-300 text-sky-500" />
+            <label for="one_time" class="ml-2 block cursor-pointer text-sm font-medium text-gray-700">
+              This was an audio-only call
+            </label>
           </div>
+
+          <div class="flex items-center">
+            <input
+              id="one_time"
+              v-model="form.reminderChoice"
+              value="one_time"
+              name="reminder-frequency"
+              type="radio"
+              class="h-4 w-4 border-gray-300 text-sky-500" />
+            <label for="one_time" class="ml-2 block cursor-pointer text-sm font-medium text-gray-700">
+              This was a video call
+            </label>
+          </div>
+        </div>
 
         <div class="border-b border-gray-200 p-5">
           <!-- cta to add a title -->
@@ -149,15 +155,32 @@
       <li v-for="call in localCalls" :key="call.id" class="item-list border-b border-gray-200 hover:bg-slate-50">
         <div v-if="editedCallId !== call.id" class="flex items-center justify-between p-3">
           <div>
-
             <!-- icon phone cancel -->
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M16 8l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2">
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M16 8l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2M5 3a2 2 0 00-2 2v1c0 8.284 6.716 15 15 15h1a2 2 0 002-2v-3.28a1 1 0 00-.684-.948l-4.493-1.498a1 1 0 00-1.21.502l-1.13 2.257a11.042 11.042 0 01-5.516-5.517l2.257-1.128a1 1 0 00.502-1.21L9.228 3.683A1 1 0 008.279 3H5z" />
             </svg>
 
             <!-- call accepted -->
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2">
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
             </svg>
           </div>
 
