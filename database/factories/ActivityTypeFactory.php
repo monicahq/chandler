8fb58afed0bec83be26da6e84f33d8ee56490870
Regis Freyd <djaiss@users.noreply.github.com>
2022-05-17 14:17:15 -0400

    feat: manage activity types

---
+++ database/factories/ActivityTypeFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Account;
+use App\Models\ActivityType;
+use App\Models\CallReasonType;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ActivityTypeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = ActivityType::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'account_id' => Account::factory(),
+            'label' => $this->faker->name(),
+        ];
+    }
+}
---
+++ tests/Unit/Models/ActivityTest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\Activity;
+use App\Models\CallReason;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class ActivityTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @test */
+    public function it_belongs_to_an_activity_type()
+    {
+        $activity = Activity::factory()->create();
+        $this->assertTrue($activity->activityType()->exists());
+    }
+}
