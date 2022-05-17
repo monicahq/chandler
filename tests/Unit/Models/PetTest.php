ae26762dadfed6509eb810e053f22b4eb6b583dd
Regis Freyd <djaiss@users.noreply.github.com>
2022-05-17 10:32:32 -0400

    feat: manage pets

---
+++ tests/Unit/Models/PetTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\Pet;
+use App\Models\PetCategory;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class PetTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    /** @test */
+    public function it_has_one_pet_category()
+    {
+        $pet = Pet::factory()->create();
+
+        $this->assertTrue($pet->petCategory()->exists());
+    }
+
+    /** @test */
+    public function it_has_one_contact()
+    {
+        $pet = Pet::factory()->create();
+
+        $this->assertTrue($pet->contact()->exists());
+    }
+}
