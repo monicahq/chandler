f54b1bdd6254e6f64784819b16a15c23f75b970f
Regis Freyd <djaiss@users.noreply.github.com>
2022-05-19 08:14:09 -0400

    Create AccountHelper.php

---
+++ app/Helpers/AccountHelper.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Helpers;
+
+use App\Models\User;
+use App\Models\Vault;
+use Illuminate\Support\Facades\DB;
+
+class AccountHelper
+{
+    /**
+     * Generate the Account short code.
+     *
+     * @return string
+     */
+    public static function generateShortCode(): string
+    {
+
+
+        return $friendlyType;
+    }
+}
