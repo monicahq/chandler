e9a449ee7c7c526e1b42da1440faede6a1c7e888
Regis Freyd <djaiss@users.noreply.github.com>
2022-06-09 21:34:44 -0400

    Update 2022_06_09_173049_create_gifts_table.php

--- database/migrations/2022_06_09_173049_create_gifts_table.php
+++ database/migrations/2022_06_09_173049_create_gifts_table.php
@@ -37,7 +37,8 @@ return new class extends Migration
         Schema::create('gifts', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('contact_id');
-            $table->string('type');
+            $table->unsignedBigInteger('gift_occasion_id')->nullable();
+            $table->unsignedBigInteger('gift_state_id')->nullable();
             $table->string('name');
             $table->text('description')->nullable();
             $table->integer('estimated_price')->nullable();
@@ -47,10 +48,20 @@ return new class extends Migration
             $table->datetime('bought_at')->nullable();
             $table->timestamps();
             $table->foreign('contact_id')->references('id')->on('contacts')->onDelete('cascade');
+            $table->foreign('gift_occasion_id')->references('id')->on('gift_occasions')->onDelete('set null');
+            $table->foreign('gift_state_id')->references('id')->on('gift_states')->onDelete('set null');
             $table->foreign('currency_id')->references('id')->on('currencies')->onDelete('set null');
         });
 
-        Schema::create('contact_gift', function (Blueprint $table) {
+        Schema::create('gift_urls', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('gift_id');
+            $table->string('url');
+            $table->timestamps();
+            $table->foreign('gift_id')->references('id')->on('gifts')->onDelete('cascade');
+        });
+
+        Schema::create('gift_recipient', function (Blueprint $table) {
             $table->unsignedBigInteger('loan_id');
             $table->unsignedBigInteger('loaner_id');
             $table->unsignedBigInteger('loanee_id');
@@ -69,6 +80,8 @@ return new class extends Migration
     public function down()
     {
         Schema::dropIfExists('gift_occasions');
+        Schema::dropIfExists('gift_states');
         Schema::dropIfExists('gifts');
+        Schema::dropIfExists('gift_urls');
     }
 };
