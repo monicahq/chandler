4a8117e4bcb8e2b7c9dd7d277780102198efa053
Regis Freyd <djaiss@users.noreply.github.com>
2022-06-26 10:40:39 -0400

    feat: i18n

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Listeners\LocaleUpdatedListener;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -17,6 +18,9 @@ class EventServiceProvider extends ServiceProvider
     protected $listen = [
         Registered::class => [
             SendEmailVerificationNotification::class,
+        ],
+        LocaleUpdated::class => [
+            LocaleUpdatedListener::class,
         ],
     ];
 
