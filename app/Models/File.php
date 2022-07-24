13512e17e04b7447459d0f2fcc196a37b90a34b4
Regis Freyd <djaiss@users.noreply.github.com>
2022-07-23 20:46:08 -0400

    feat: documents

---
+++ app/Models/File.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class File extends Model
+{
+    use HasFactory;
+
+    protected $table = 'files';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'contact_id',
+        'uuid',
+        'original_url',
+        'cdn_url',
+        'name',
+        'mime_type',
+        'type',
+        'size',
+    ];
+
+    /**
+     * The event map for the model.
+     *
+     * @var array
+     */
+    protected $dispatchesEvents = [
+        'deleted' => FileDeleted::class,
+    ];
+
+    /**
+     * Get the contact associated with the file.
+     *
+     * @return BelongsTo
+     */
+    public function contact(): BelongsTo
+    {
+        return $this->belongsTo(Contact::class);
+    }
+}
