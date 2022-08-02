8e7d6fb9702fddd53cf47de3ccbc258f02c90c59
Alexis Saettler <alexis@saettler.org>
2022-08-02 23:24:04 +0200

    fix langs

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 import basicSsl from '@vitejs/plugin-basic-ssl';
+import i18n from 'laravel-vue-i18n/vite';
 
 export default defineConfig(({ mode }) => {
   // Load env file based on `mode` in the current working directory.
@@ -21,6 +22,7 @@ export default defineConfig(({ mode }) => {
           },
         },
       }),
+      i18n(),
       basicSsl(),
     ],
     server: {
