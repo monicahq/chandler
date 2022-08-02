93d38367895280e07fa4b64311f35329653325e5
Alexis Saettler <alexis@saettler.org>
2022-08-02 22:43:31 +0200

    add APP_TRUSTED_PROXIES

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -8,13 +8,6 @@ use Illuminate\Http\Request;
 class TrustProxies extends Middleware
 {
     /**
-     * The trusted proxies for this application.
-     *
-     * @var array|string|null
-     */
-    protected $proxies = '*';
-
-    /**
      * The headers that should be used to detect proxies.
      *
      * @var int
@@ -25,4 +18,14 @@ class TrustProxies extends Middleware
         Request::HEADER_X_FORWARDED_PORT |
         Request::HEADER_X_FORWARDED_PROTO |
         Request::HEADER_X_FORWARDED_AWS_ELB;
+
+    /**
+     * Get the trusted proxies.
+     *
+     * @return array|string|null
+     */
+    protected function proxies()
+    {
+        return config('app.trust_proxies');
+    }
 }
