6e31014c6b06597aa996f641a282798fbb8b4489
Alexis Saettler <alexis@saettler.org>
2022-08-29 00:00:49 +0200

    fix

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DATABASE=database/database.sqlite
+
+chmod a+x /root && sudo rm -rf /var/www/html && sudo ln -s "$(pwd)/public" /var/www/html
+cp .env.example .env && echo "APP_TRUSTED_PROXIES=*" >> .env
+sed -i "s%DB_CONNECTION=.*%DB_CONNECTION=sqlite%" .env
+sed -i "s%DB_DATABASE=.*%DB_DATABASE=$(pwd)/$DATABASE%" .env
+touch $DATABASE && chgrp www-data database $DATABASE && chmod g+w database $DATABASE
+chgrp -R www-data storage && chmod -R g+w storage
+
+composer install
+php artisan key:generate --no-interaction
+php artisan monica:setup -vvv
+yarn run install
+yarn run build
+
+a2enmod rewrite
+apache2ctl restart
