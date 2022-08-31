fd3160387208f75cc855b7aedae2d164f5411ef6
Mazarin <djaiss@users.noreply.github.com>
2022-08-31 07:44:05 -0400

    Update resources/js/Shared/Modules/FeedItems/GenericAction.vue
    Co-authored-by: Alexis Saettler <alexis@saettler.org>

--- resources/js/Shared/Modules/FeedItems/GenericAction.vue
+++ resources/js/Shared/Modules/FeedItems/GenericAction.vue
@@ -3,7 +3,7 @@
     <avatar :data="data.avatar" :classes="'rounded-full border-gray-200 dark:border-gray-800 border relative h-8 w-8 mr-2'" />
 
     <div class="flex flex-col">
-      <inertia-link :href="data.url" class="text-gray-800 hover:underline">{{ data.name }}</inertia-link>
+      <inertia-link :href="data.url" class="text-gray-800 dark:text-gray-200 hover:underline">{{ data.name }}</inertia-link>
       <span v-if="data.age" class="text-gray-500">{{ data.age }}</span>
       <span v-else class="text-xs text-gray-500">Unknown age</span>
     </div>
