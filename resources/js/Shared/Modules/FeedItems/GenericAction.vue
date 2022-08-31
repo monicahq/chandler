fac36147e183eb97176d4cfeb7799c72a7243a83
Mazarin <djaiss@users.noreply.github.com>
2022-08-31 07:43:55 -0400

    Update resources/js/Shared/Modules/FeedItems/GenericAction.vue
    Co-authored-by: Alexis Saettler <alexis@saettler.org>

--- resources/js/Shared/Modules/FeedItems/GenericAction.vue
+++ resources/js/Shared/Modules/FeedItems/GenericAction.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-if="!contactViewMode" class="flex items-center rounded-lg border border-gray-300 px-3 py-2">
+  <div v-if="!contactViewMode" class="flex items-center rounded-lg border border-gray-300 dark:border-gray-700 px-3 py-2">
     <avatar :data="data.avatar" :classes="'rounded-full border-gray-200 border relative h-8 w-8 mr-2'" />
 
     <div class="flex flex-col">
