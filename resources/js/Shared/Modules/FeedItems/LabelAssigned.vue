b1d553269adc892db2edd90a25a24252f6318361
Mazarin <djaiss@users.noreply.github.com>
2022-08-31 07:44:16 -0400

    Update resources/js/Shared/Modules/FeedItems/LabelAssigned.vue
    Co-authored-by: Alexis Saettler <alexis@saettler.org>

--- resources/js/Shared/Modules/FeedItems/LabelAssigned.vue
+++ resources/js/Shared/Modules/FeedItems/LabelAssigned.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="rounded-lg border border-gray-300 dark:border-gray-700">
     <!-- contact information -->
-    <div v-if="!contactViewMode" class="flex items-center border-b border-gray-300 px-3 py-2 text-sm">
+    <div v-if="!contactViewMode" class="flex items-center border-b border-gray-300 dark:border-gray-700 px-3 py-2 text-sm">
       <avatar :data="data.contact.avatar" :classes="'rounded-full border-gray-200 border relative h-5 w-5 mr-2'" />
 
       <div class="flex flex-col">
