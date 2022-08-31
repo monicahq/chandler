7b8ed31a3d8a00015e4a11861ecafb3b1557f326
Mazarin <djaiss@users.noreply.github.com>
2022-08-31 07:44:00 -0400

    Update resources/js/Shared/Modules/FeedItems/GenericAction.vue
    Co-authored-by: Alexis Saettler <alexis@saettler.org>

--- resources/js/Shared/Modules/FeedItems/GenericAction.vue
+++ resources/js/Shared/Modules/FeedItems/GenericAction.vue
@@ -1,6 +1,6 @@
 <template>
   <div v-if="!contactViewMode" class="flex items-center rounded-lg border border-gray-300 dark:border-gray-700 px-3 py-2">
-    <avatar :data="data.avatar" :classes="'rounded-full border-gray-200 border relative h-8 w-8 mr-2'" />
+    <avatar :data="data.avatar" :classes="'rounded-full border-gray-200 dark:border-gray-800 border relative h-8 w-8 mr-2'" />
 
     <div class="flex flex-col">
       <inertia-link :href="data.url" class="text-gray-800 hover:underline">{{ data.name }}</inertia-link>
