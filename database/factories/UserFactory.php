2a664c981638ad6021b66f266e59670c1fabc650
Alexis Saettler <alexis@saettler.org>
2022-09-11 19:00:47 +0200

    fix phpstan

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,7 +12,7 @@ class UserFactory extends Factory
     /**
      * The name of the factory's corresponding model.
      *
-     * @var string
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
      */
     protected $model = User::class;
 
