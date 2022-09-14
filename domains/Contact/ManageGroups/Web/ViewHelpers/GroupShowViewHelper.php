069ff906bd36a781741df93461ae37ec66a9c2a1
Regis Freyd <djaiss@users.noreply.github.com>
2022-09-14 16:37:16 -0400

    feat: detail of a group

--- domains/Contact/ManageGroups/Web/ViewHelpers/GroupShowViewHelper.php
+++ domains/Contact/ManageGroups/Web/ViewHelpers/GroupShowViewHelper.php
@@ -4,20 +4,23 @@ namespace App\Contact\ManageGroups\Web\ViewHelpers;
 
 use App\Models\Contact;
 use App\Models\Group;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 
 class GroupShowViewHelper
 {
     /**
      * Gets all the contacts in this group.
-     * A group has a type. The type may have one or more roles.
+     * A group has a mandatory type. This type may have one or more roles.
+     * Contacts can be assigned to roles, but it's not mandatory.
      * So we need to group contacts by roles if they exist, or list them
      * alphabetically otherwise.
      *
      * @param  Group  $group
+     * @param  User  $user
      * @return array
      */
-    public static function data(Group $group): array
+    public static function data(Group $group, User $user): array
     {
         $roles = $group->groupType->groupTypeRoles()->orderBy('position')->get();
 
@@ -34,9 +37,14 @@ class GroupShowViewHelper
                     $contact = Contact::find($contact->contact_id);
 
                     $contactsCollection->push([
+                        'id' => $contact->id,
                         'name' => $contact->name,
-                        'avatar' => $contact->avatar,
                         'age' => $contact->age,
+                        'avatar' => $contact->avatar,
+                        'url' => route('contact.show', [
+                            'vault' => $contact->vault_id,
+                            'contact' => $contact->id,
+                        ]),
                     ]);
                 }
 
@@ -59,20 +67,29 @@ class GroupShowViewHelper
             $contact = Contact::find($contact->contact_id);
 
             $contactsCollection->push([
+                'id' => $contact->id,
                 'name' => $contact->name,
-                'avatar' => $contact->avatar,
                 'age' => $contact->age,
+                'avatar' => $contact->avatar,
+                'url' => route('contact.show', [
+                    'vault' => $contact->vault_id,
+                    'contact' => $contact->id,
+                ]),
             ]);
         }
-        $rolesCollection->push([
-            'id' => $rolesCollection->count() + 1,
-            'label' => 'No role',
-            'contacts' => $contactsCollection,
-        ]);
+        // only adds this row if there is at least one contact
+        if ($contactsCollection->count() > 0) {
+            $rolesCollection->push([
+                'id' => $rolesCollection->count() + 1,
+                'label' => 'No role',
+                'contacts' => $contactsCollection,
+            ]);
+        }
 
         return [
             'id' => $group->id,
             'name' => $group->name,
+            'contact_count' => $group->contacts->count(),
             'type' => [
                 'label' => $group->groupType->label,
             ],
