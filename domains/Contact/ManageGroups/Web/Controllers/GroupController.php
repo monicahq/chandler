844c8c0fcd7d6b8b711c0e297419c6d0244d161e
Regis Freyd <djaiss@users.noreply.github.com>
2022-09-14 16:40:14 -0400

    fix

--- domains/Contact/ManageGroups/Web/Controllers/GroupController.php
+++ domains/Contact/ManageGroups/Web/Controllers/GroupController.php
@@ -2,9 +2,7 @@
 
 namespace App\Contact\ManageGroups\Web\Controllers;
 
-use App\Contact\ManageContact\Web\ViewHelpers\ContactIndexViewHelper;
 use App\Contact\ManageGroups\Web\ViewHelpers\GroupShowViewHelper;
-use App\Helpers\PaginatorHelper;
 use App\Http\Controllers\Controller;
 use App\Models\Group;
 use App\Models\Vault;
@@ -17,18 +15,6 @@ class GroupController extends Controller
 {
     public function index(Request $request, int $vaultId)
     {
-        $vault = Vault::findOrFail($vaultId);
-
-        $groups = Group::where('vault_id', $request->route()->parameter('vault'))
-            ->where('listed', true)
-            ->orderBy('created_at', 'asc')
-            ->paginate(25);
-
-        return Inertia::render('Vault/Contact/Index', [
-            'layoutData' => VaultIndexViewHelper::layoutData($vault),
-            'data' => ContactIndexViewHelper::data($contacts, $vault),
-            'paginator' => PaginatorHelper::getData($contacts),
-        ]);
     }
 
     public function show(Request $request, int $vaultId, int $groupId)
