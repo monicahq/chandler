37657f4beae0cc4790365f6fe54a86adbc7daa12
Regis Freyd <djaiss@users.noreply.github.com>
2022-09-22 07:14:24 -0400

    fix

--- resources/js/Pages/Vault/Journal/Create.vue
+++ resources/js/Pages/Vault/Journal/Create.vue
@@ -1,6 +1,5 @@
 <script setup>
 import Layout from '@/Shared/Layout.vue';
-import { onMounted, ref } from 'vue';
 import { useForm } from '@inertiajs/inertia-vue3';
 import PrettyLink from '@/Shared/Form/PrettyLink.vue';
 import PrettyButton from '@/Shared/Form/PrettyButton.vue';
@@ -13,16 +12,9 @@ const props = defineProps({
   data: Object,
 });
 
-const inputName = ref(null);
-
 const form = useForm({
   name: '',
   description: '',
-});
-
-onMounted(() => {
-  //inputName.focus();
-  console.log('test');
 });
 
 const submit = () => {
@@ -85,7 +77,6 @@ const submit = () => {
               :input-class="'block w-full'"
               :required="true"
               :maxlength="255"
-              ref="inputName"
               :label="$t('vault.journal_create_name')" />
 
             <text-area
