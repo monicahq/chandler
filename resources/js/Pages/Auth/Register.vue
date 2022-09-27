5f948b402121e58bb2c67ef575e2871110c7ae0e
Alexis Saettler <alexis@saettler.org>
2022-09-27 23:08:59 +0200

    style: update Auth pages

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,14 +1,38 @@
+<script setup>
+import { Link, useForm } from '@inertiajs/inertia-vue3';
+import JetGuestLayout from '@/Shared/Guest.vue';
+import JetInput from '@/Components/Input.vue';
+import JetLabel from '@/Components/Label.vue';
+import JetValidationErrors from '@/Components/ValidationErrors.vue';
+import PrettyButton from '@/Shared/Form/PrettyButton.vue';
+
+const form = useForm({
+  first_name: '',
+  last_name: '',
+  email: '',
+  password: '',
+  password_confirmation: '',
+  terms: false,
+});
+
+const submit = () => {
+  form.post(route('register'), {
+    onFinish: () => form.reset('password', 'password_confirmation'),
+  });
+};
+</script>
+
 <template>
-  <div>
-    <breeze-validation-errors class="mb-4" />
+  <JetGuestLayout>
+    <JetValidationErrors class="mb-4" />
 
     <p class="mb-2 text-lg font-bold">Sign up for an account</p>
     <p class="mb-8 text-sm text-gray-500">Your name here will be used to add yourself as a contact.</p>
 
     <form @submit.prevent="submit">
       <div>
-        <breeze-label for="first_name" value="First name" />
-        <breeze-input
+        <JetLabel for="first_name" value="First name" />
+        <JetInput
           id="first_name"
           v-model="form.first_name"
           type="text"
@@ -19,8 +43,8 @@
       </div>
 
       <div class="mt-4">
-        <breeze-label for="last_name" value="Last name" />
-        <breeze-input
+        <JetLabel for="last_name" value="Last name" />
+        <JetInput
           id="last_name"
           v-model="form.last_name"
           type="text"
@@ -30,8 +54,8 @@
       </div>
 
       <div class="mt-4">
-        <breeze-label for="email" value="Email" />
-        <breeze-input
+        <JetLabel for="email" value="Email" />
+        <JetInput
           id="email"
           v-model="form.email"
           type="email"
@@ -41,8 +65,8 @@
       </div>
 
       <div class="mt-4">
-        <breeze-label for="password" value="Password" />
-        <breeze-input
+        <JetLabel for="password" value="Password" />
+        <JetInput
           id="password"
           v-model="form.password"
           type="password"
@@ -52,8 +76,8 @@
       </div>
 
       <div class="mt-4 mb-8">
-        <breeze-label for="password_confirmation" value="Confirm Password" />
-        <breeze-input
+        <JetLabel for="password_confirmation" value="Confirm Password" />
+        <JetInput
           id="password_confirmation"
           v-model="form.password_confirmation"
           type="password"
@@ -63,51 +87,12 @@
       </div>
 
       <div class="mt-4 flex items-center justify-end">
-        <inertia-link :href="route('login')" class="mr-4 text-sm text-blue-500 hover:underline dark:text-gray-400">
+        <Link :href="route('login')" class="mr-4 text-sm text-blue-500 hover:underline dark:text-gray-400">
           Already registered?
-        </inertia-link>
+        </Link>
 
         <PrettyButton :text="'Register'" :state="loadingState" :classes="'save'" />
       </div>
     </form>
-  </div>
+  </JetGuestLayout>
 </template>
-
-<script>
-import BreezeGuestLayout from '@/Shared/Guest.vue';
-import BreezeInput from '@/Components/Input.vue';
-import BreezeLabel from '@/Components/Label.vue';
-import BreezeValidationErrors from '@/Components/ValidationErrors.vue';
-import PrettyButton from '@/Shared/Form/PrettyButton.vue';
-
-export default {
-  components: {
-    BreezeInput,
-    BreezeLabel,
-    BreezeValidationErrors,
-    PrettyButton,
-  },
-  layout: BreezeGuestLayout,
-
-  data() {
-    return {
-      form: this.$inertia.form({
-        first_name: '',
-        last_name: '',
-        email: '',
-        password: '',
-        password_confirmation: '',
-        terms: false,
-      }),
-    };
-  },
-
-  methods: {
-    submit() {
-      this.form.post(this.route('register'), {
-        onFinish: () => this.form.reset('password', 'password_confirmation'),
-      });
-    },
-  },
-};
-</script>
