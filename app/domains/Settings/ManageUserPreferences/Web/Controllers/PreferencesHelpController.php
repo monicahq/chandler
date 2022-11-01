1b55fdf96a6dedff9af20bd824032bd29dd2da86
Regis Freyd <djaiss@users.noreply.github.com>
2022-10-31 20:51:16 -0400

    chore: move domains folder to app folder

---
+++ app/domains/Settings/ManageUserPreferences/Web/Controllers/PreferencesHelpController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Domains\Settings\ManageUserPreferences\Web\Controllers;
+
+use App\Domains\Settings\ManageUserPreferences\Services\StoreHelpPreference;
+use App\Domains\Settings\ManageUserPreferences\Web\ViewHelpers\UserPreferencesIndexViewHelper;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class PreferencesHelpController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request = [
+            'account_id' => Auth::user()->account_id,
+            'author_id' => Auth::user()->id,
+            'visibility' => ! Auth::user()->help_shown,
+        ];
+
+        (new StoreHelpPreference())->execute($request);
+
+        return response()->json([
+            'data' => UserPreferencesIndexViewHelper::dtoHelp(Auth::user()),
+        ], 200);
+    }
+}
