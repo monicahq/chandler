fc118c8d937da65f5d80be9ae6b8a61f8030af49
Regis Freyd <djaiss@users.noreply.github.com>
2022-11-05 07:24:02 -0400

    Update SetupAccount.php

--- app/Domains/Settings/CreateAccount/Services/SetupAccount.php
+++ app/Domains/Settings/CreateAccount/Services/SetupAccount.php
@@ -269,16 +269,16 @@ class SetupAccount extends QueuableService implements ServiceInterface
 
         // religions
         $module = (new CreateModule())->execute([
-            'account_id' => $this->user->account_id,
-            'author_id' => $this->user->id,
+            'account_id' => $this->author->account_id,
+            'author_id' => $this->author->id,
             'name' => trans('app.module_religions'),
             'type' => Module::TYPE_RELIGIONS,
             'can_be_deleted' => false,
             'reserved_to_contact_information' => true,
         ]);
         (new AssociateModuleToTemplatePage())->execute([
-            'account_id' => $this->user->account_id,
-            'author_id' => $this->user->id,
+            'account_id' => $this->author->account_id,
+            'author_id' => $this->author->id,
             'template_id' => $this->template->id,
             'template_page_id' => $templatePageContact->id,
             'module_id' => $module->id,
