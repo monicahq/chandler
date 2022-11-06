15ec7570410268e2ba8bdddde3b8e03b57356955
Fabian Batioja <f.batioja@uniandes.edu.co>
2022-11-05 20:00:16 -0500

    pr2

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,6 +30,6 @@ class Kernel extends ConsoleKernel
     {
         $this->load(__DIR__.'/Commands');
 
-        require base_path('routes/console.php');
+        require_once base_path('routes/console.php');
     }
 }
