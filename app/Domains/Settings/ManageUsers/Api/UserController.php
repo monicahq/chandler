4610fe9a43556c2fccfc008aa889ab2d4567a90e
Alexis Saettler <alexis@saettler.org>
2022-11-08 22:57:12 +0100

    move controller

--- app/Domains/Settings/ManageUsers/Api/UserController.php
+++ app/Domains/Settings/ManageUsers/Api/UserController.php
@@ -1,11 +1,15 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Domains\Settings\ManageUsers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Spatie\RouteAttributes\Attributes\Get;
+use Spatie\RouteAttributes\Attributes\Middleware;
+use Spatie\RouteAttributes\Attributes\Prefix;
 
+#[Middleware('api')]
+#[Prefix('api')]
 class UserController extends Controller
 {
     #[Get('user', middleware: 'abilities:read')]
