8b2975440b6568685b60b876fe3f9ecd0cd84011
Alexis Saettler <alexis@saettler.org>
2022-11-08 22:44:01 +0100

    rename

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -5,7 +5,7 @@ return [
     'documentations' => [
         'default' => [
             'api' => [
-                'title' => 'L5 Swagger UI',
+                'title' => 'Monica API',
             ],
 
             'routes' => [
