5502823dfeb7676c59bad5becaea95ab93f31f0f
Alexis Saettler <alexis@saettler.org>
2022-11-29 21:23:00 +0100

    fix coverage

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,8 @@ class Kernel extends ConsoleKernel
      *
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
      * @return void
+     *
+     * @codeCoverageIgnore
      */
     protected function schedule(Schedule $schedule)
     {
