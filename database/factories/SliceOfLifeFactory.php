c7f43f48716ef0b91fb1eaf3f2bca6889109a3b6
Regis Freyd <djaiss@users.noreply.github.com>
2022-12-28 21:46:00 -0500

    Update SliceOfLifeFactory.php

--- database/factories/SliceOfLifeFactory.php
+++ database/factories/SliceOfLifeFactory.php
@@ -6,13 +6,11 @@ use App\Models\Journal;
 use App\Models\SliceOfLife;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Religion>
+ */
 class SliceOfLifeFactory extends Factory
 {
-    /**
-     * The name of the factory's corresponding model.
-     *
-     * @var class-string<\Illuminate\Database\Eloquent\Model>
-     */
     protected $model = SliceOfLife::class;
 
     /**
