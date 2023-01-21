a2cee4e08f1b3f1ce31bb391887d88d0e96ae813
Regis Freyd <djaiss@users.noreply.github.com>
2023-01-21 18:03:11 -0500

    fix

--- resources/js/Shared/Modules/FeedItems/MoodTrackingEvent.vue
+++ resources/js/Shared/Modules/FeedItems/MoodTrackingEvent.vue
@@ -17,12 +17,30 @@
 
     <div class="px-3 py-2">
       <!-- the mood tracking event still exists in the system -->
-      <div v-if="data.mood_tracking_event.object" class="flex items-center">
-        <span class="mr-2">{{ data.mood_tracking_event.description }}</span>
-        <span class="mr-2"><span class="text-gray-500">for</span> {{ data.mood_tracking_event.object.rated_at }}</span>
-        <span v-if="data.mood_tracking_event.object.number_of_hours_slept"
-          >and slept {{ data.mood_tracking_event.object.number_of_hours_slept }} hours</span
-        >
+      <div v-if="data.mood_tracking_event.object">
+
+        <div class="flex mb-2">
+          <div class="mr-2">
+            {{ data.mood_tracking_event.description }}
+          </div>
+
+          <!-- slept for -->
+          <div v-if="data.mood_tracking_event.object.number_of_hours_slept" class="flex items-center">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-gray-400 mr-1">
+              <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+            </svg>
+
+            <span>{{ data.mood_tracking_event.object.number_of_hours_slept }} hours</span>
+          </div>
+        </div>
+
+        <!-- date -->
+        <div class="flex items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-gray-400 mr-1">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5" />
+          </svg>
+          {{ data.mood_tracking_event.object.rated_at }}
+        </div>
       </div>
 
       <!-- the mood tracking event was deleted -->
