2fbb69a49315ef72fad150edc950ed4085fae2bf
MonicaBot <MonicaBot@users.noreply.github.com>
2023-01-29 15:09:58 +0000

    chore: javascript linting with eslint and prettier

--- resources/js/Shared/Modules/CreateLifeEvent.vue
+++ resources/js/Shared/Modules/CreateLifeEvent.vue
@@ -98,20 +98,35 @@ const store = () => {
       @submit.prevent="store()">
       <!-- choose life event categories/types -->
       <div v-if="!selectedLifeEventType" class="border-b border-gray-200 dark:border-gray-700">
-        <div class="grid grid-skeleton grid-cols-2 gap-2 justify-center p-3">
+        <div class="grid-skeleton grid grid-cols-2 justify-center gap-2 p-3">
           <!-- choose a life event type -->
           <div>
-            <p class="text-xs font-semibold mb-1">Categories</p>
+            <p class="mb-1 text-xs font-semibold">Categories</p>
             <ul class="rounded-lg border border-gray-200 bg-white dark:border-gray-700 dark:bg-gray-900">
-              <li @click="loadTypes(category)" v-for="category in data.life_event_categories" :key="category.id" class="item-list flex border-b border-gray-200 px-3 py-1 hover:bg-slate-50 dark:border-gray-700 dark:bg-slate-900 hover:dark:bg-slate-800 cursor-pointer">
-                <div class="flex justify-between w-full" :class="category.id === selectedLifeEventCategory.id ? 'font-bold' : ''">
+              <li
+                @click="loadTypes(category)"
+                v-for="category in data.life_event_categories"
+                :key="category.id"
+                class="item-list flex cursor-pointer border-b border-gray-200 px-3 py-1 hover:bg-slate-50 dark:border-gray-700 dark:bg-slate-900 hover:dark:bg-slate-800">
+                <div
+                  class="flex w-full justify-between"
+                  :class="category.id === selectedLifeEventCategory.id ? 'font-bold' : ''">
                   <!-- label category -->
                   <div>{{ category.label }}</div>
 
                   <!-- arrow -->
                   <span v-if="category.id !== selectedLifeEventCategory.id">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-gray-600">
-                      <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      class="h-6 w-6 text-gray-600">
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
                     </svg>
                   </span>
                 </div>
@@ -121,11 +136,16 @@ const store = () => {
 
           <!-- list of life event types -->
           <div>
-            <p class="text-xs font-semibold mb-1">Types</p>
+            <p class="mb-1 text-xs font-semibold">Types</p>
             <ul class="rounded-lg border border-gray-200 bg-white dark:border-gray-700 dark:bg-gray-900">
-              <li v-for="lifeEventType in selectedLifeEventCategory.life_event_types" :key="lifeEventType.id" class="item-list flex justify-between border-b border-gray-200 px-3 py-1 hover:bg-slate-50 dark:border-gray-700 dark:bg-slate-900 hover:dark:bg-slate-800 cursor-pointer">
+              <li
+                v-for="lifeEventType in selectedLifeEventCategory.life_event_types"
+                :key="lifeEventType.id"
+                class="item-list flex cursor-pointer justify-between border-b border-gray-200 px-3 py-1 hover:bg-slate-50 dark:border-gray-700 dark:bg-slate-900 hover:dark:bg-slate-800">
                 <span>{{ lifeEventType.label }}</span>
-                <span @click="chooseType(lifeEventType)" class="text-blue-500 hover:underline text-sm">{{ $t('app.choose') }}</span>
+                <span @click="chooseType(lifeEventType)" class="text-sm text-blue-500 hover:underline">{{
+                  $t('app.choose')
+                }}</span>
               </li>
             </ul>
           </div>
@@ -133,30 +153,35 @@ const store = () => {
       </div>
 
       <!-- type has been selected -->
-      <div v-else class="flex items-center justify-between border-b border-gray-200 dark:border-gray-700 p-3">
+      <div v-else class="flex items-center justify-between border-b border-gray-200 p-3 dark:border-gray-700">
         <div>
-          <span class="text-sm">Chosen type:</span> <span class="px-2 py-1 text-sm border bg-white font-mono rounded">{{ selectedLifeEventCategory.label }}</span> > <span class="px-2 py-1 text-sm border bg-white font-mono rounded">{{ selectedLifeEventType.label }}</span>
+          <span class="text-sm">Chosen type:</span>
+          <span class="rounded border bg-white px-2 py-1 font-mono text-sm">{{ selectedLifeEventCategory.label }}</span>
+          > <span class="rounded border bg-white px-2 py-1 font-mono text-sm">{{ selectedLifeEventType.label }}</span>
         </div>
 
-        <p @click="resetType()" class="text-blue-500 hover:underline cursor-pointer text-sm">{{ $t('app.change') }}</p>
+        <p @click="resetType()" class="cursor-pointer text-sm text-blue-500 hover:underline">{{ $t('app.change') }}</p>
       </div>
 
       <!-- date of the event -->
-      <div v-if="selectedLifeEventType" class="border-b border-gray-200 dark:border-gray-700 p-3">
-
+      <div v-if="selectedLifeEventType" class="border-b border-gray-200 p-3 dark:border-gray-700">
         <!-- default date -->
         <div v-if="!editDate" class="flex items-center justify-between">
-          <div>
-            <span class="text-sm">Date of the event:</span> {{ props.data.current_date_human_format }}
-          </div>
+          <div><span class="text-sm">Date of the event:</span> {{ props.data.current_date_human_format }}</div>
 
-          <p @click="editDate = true" class="text-blue-500 hover:underline cursor-pointer text-sm">{{ $t('app.change') }}</p>
+          <p @click="editDate = true" class="cursor-pointer text-sm text-blue-500 hover:underline">
+            {{ $t('app.change') }}
+          </p>
         </div>
 
         <!-- customize date -->
         <div v-if="editDate">
           <p class="mb-2 block text-sm dark:text-gray-100">Date of the event</p>
-          <v-date-picker v-model="form.started_at" :timezone="'UTC'" class="inline-block h-full" :model-config="modelConfig">
+          <v-date-picker
+            v-model="form.started_at"
+            :timezone="'UTC'"
+            class="inline-block h-full"
+            :model-config="modelConfig">
             <template #default="{ inputValue, inputEvents }">
               <input
                 class="rounded border bg-white px-2 py-1 dark:bg-gray-900"
@@ -168,11 +193,11 @@ const store = () => {
       </div>
 
       <!-- participants -->
-      <div v-if="selectedLifeEventType" class="border-b border-gray-200 dark:border-gray-700 p-3">
+      <div v-if="selectedLifeEventType" class="border-b border-gray-200 p-3 dark:border-gray-700">
         <p class="mb-2 block text-sm dark:text-gray-100">Participants</p>
 
         <!-- current contact -->
-        <div class="flex items-center mb-4">
+        <div class="mb-4 flex items-center">
           <avatar :data="props.data.contact.avatar" :classes="'mr-2 h-5 w-5'" />
 
           <span>{{ props.data.contact.name }}</span>
@@ -180,17 +205,17 @@ const store = () => {
 
         <!-- all other participants -->
         <contact-selector
-            v-model="form.participants"
-            :search-url="layoutData.vault.url.search_contacts_only"
-            :most-consulted-contacts-url="layoutData.vault.url.get_most_consulted_contacts"
-            :display-most-consulted-contacts="true"
-            :add-multiple-contacts="true"
-            :required="true"
-            :div-outer-class="'flex-1 border-gray-200 dark:border-gray-700'" />
+          v-model="form.participants"
+          :search-url="layoutData.vault.url.search_contacts_only"
+          :most-consulted-contacts-url="layoutData.vault.url.get_most_consulted_contacts"
+          :display-most-consulted-contacts="true"
+          :add-multiple-contacts="true"
+          :required="true"
+          :div-outer-class="'flex-1 border-gray-200 dark:border-gray-700'" />
       </div>
 
       <!-- options -->
-      <div v-if="selectedLifeEventType" class="border-b border-gray-200 dark:border-gray-700 p-3">
+      <div v-if="selectedLifeEventType" class="border-b border-gray-200 p-3 dark:border-gray-700">
         <span
           class="mr-2 mb-2 flex cursor-pointer flex-wrap rounded-lg border bg-slate-200 px-1 py-1 hover:bg-slate-300 dark:text-gray-900"
           @click="displayMiddleNameField">
@@ -198,9 +223,14 @@ const store = () => {
         </span>
       </div>
       <div class="flex justify-between p-5">
-          <pretty-span :text="$t('app.cancel')" :classes="'mr-3'" @click="$emit('closeModal')" />
-          <pretty-button v-if="selectedLifeEventType" :text="$t('app.save')" :state="loadingState" :icon="'plus'" :classes="'save'" />
-        </div>
+        <pretty-span :text="$t('app.cancel')" :classes="'mr-3'" @click="$emit('closeModal')" />
+        <pretty-button
+          v-if="selectedLifeEventType"
+          :text="$t('app.save')"
+          :state="loadingState"
+          :icon="'plus'"
+          :classes="'save'" />
+      </div>
     </form>
   </div>
 </template>
--- resources/js/Shared/Modules/LifeEvent.vue
+++ resources/js/Shared/Modules/LifeEvent.vue
@@ -67,21 +67,37 @@ const toggleLifeEventVisibility = (lifeEvent) => {
     <div class="mb-3 items-center justify-between border-b border-gray-200 pb-2 dark:border-gray-700 sm:flex">
       <div class="mb-2 sm:mb-0">
         <span class="relative mr-1">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-sidebar relative inline h-4 w-4">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="icon-sidebar relative inline h-4 w-4">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z" />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z" />
           </svg>
-
         </span>
 
         <span class="font-semibold"> Life events </span>
       </div>
-      <pretty-button :text="'Add a life event'" :icon="'plus'" :classes="'sm:w-fit w-full'" @click="showCreateLifeEventModal" />
+      <pretty-button
+        :text="'Add a life event'"
+        :icon="'plus'"
+        :classes="'sm:w-fit w-full'"
+        @click="showCreateLifeEventModal" />
     </div>
 
     <div>
       <!-- add a timeline event -->
-      <create-life-event :data="props.data"
+      <create-life-event
+        :data="props.data"
         :layout-data="props.layoutData"
         :open-modal="createLifeEventModalShown"
         :create-timeline-event="true"
@@ -91,62 +107,113 @@ const toggleLifeEventVisibility = (lifeEvent) => {
       <!-- list of timeline events -->
       <div v-if="timeline">
         <div v-for="timelineEvent in timeline" :key="timelineEvent.id" class="mb-4">
-
           <!-- timeline event name -->
-          <div class="flex justify-between items-center border border-gray-200 hover:bg-slate-50 dark:border-gray-700 dark:bg-slate-900 hover:dark:bg-slate-800 rounded-lg px-3 py-2 mb-2 cursor-pointer" @click="toggleTimelineEventVisibility(timelineEvent)">
-
+          <div
+            class="mb-2 flex cursor-pointer items-center justify-between rounded-lg border border-gray-200 px-3 py-2 hover:bg-slate-50 dark:border-gray-700 dark:bg-slate-900 hover:dark:bg-slate-800"
+            @click="toggleTimelineEventVisibility(timelineEvent)">
             <!-- timeline date / label / number of events -->
             <div>
               <span class="mr-2 text-gray-500">{{ timelineEvent.happened_at }}</span>
 
-              <span class="ml-3 whitespace-nowrap rounded-lg bg-slate-100 py-0.5 px-2 text-sm text-slate-400">{{ timelineEvent.life_events.length }}</span>
+              <span class="ml-3 whitespace-nowrap rounded-lg bg-slate-100 py-0.5 px-2 text-sm text-slate-400">{{
+                timelineEvent.life_events.length
+              }}</span>
             </div>
 
             <!-- chevrons -->
-            <svg v-if="timelineEvent.collapsed" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-gray-400">
+            <svg
+              v-if="timelineEvent.collapsed"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="h-4 w-4 text-gray-400">
               <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
             </svg>
 
-            <svg v-if="!timelineEvent.collapsed" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-gray-400">
+            <svg
+              v-if="!timelineEvent.collapsed"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="h-4 w-4 text-gray-400">
               <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
             </svg>
-
           </div>
 
           <!-- life events -->
           <div v-if="!timelineEvent.collapsed">
-            <div v-for="lifeEvent in timelineEvent.life_events" :key="lifeEvent.id" :class="!lifeEvent.collapsed ? 'border' : ''" class="ml-6 border-gray-200 rounded-lg mb-2">
+            <div
+              v-for="lifeEvent in timelineEvent.life_events"
+              :key="lifeEvent.id"
+              :class="!lifeEvent.collapsed ? 'border' : ''"
+              class="ml-6 mb-2 rounded-lg border-gray-200">
               <!-- name of life event -->
-              <div :class="lifeEvent.collapsed ? 'rounded-lg border' : ''" class="flex justify-between items-center border-b border-gray-200 hover:bg-slate-50 dark:border-gray-700 dark:bg-slate-900 hover:dark:bg-slate-800 px-3 py-2 cursor-pointer" @click="toggleLifeEventVisibility(lifeEvent)">
+              <div
+                :class="lifeEvent.collapsed ? 'rounded-lg border' : ''"
+                class="flex cursor-pointer items-center justify-between border-b border-gray-200 px-3 py-2 hover:bg-slate-50 dark:border-gray-700 dark:bg-slate-900 hover:dark:bg-slate-800"
+                @click="toggleLifeEventVisibility(lifeEvent)">
                 <!-- title -->
                 <div class="flex items-center">
                   <p class="mr-4 text-sm font-bold">Activity #1</p>
                   <div>
-                    <span class="px-2 py-1 text-sm border bg-white font-mono rounded">{{ lifeEvent.life_event_type.category.label }}</span> > <span class="px-2 py-1 text-sm border bg-white font-mono rounded">{{ lifeEvent.life_event_type.label }}</span>
+                    <span class="rounded border bg-white px-2 py-1 font-mono text-sm">{{
+                      lifeEvent.life_event_type.category.label
+                    }}</span>
+                    >
+                    <span class="rounded border bg-white px-2 py-1 font-mono text-sm">{{
+                      lifeEvent.life_event_type.label
+                    }}</span>
                   </div>
                 </div>
 
-                  <!-- chevrons -->
-                <svg v-if="lifeEvent.collapsed" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-gray-400">
+                <!-- chevrons -->
+                <svg
+                  v-if="lifeEvent.collapsed"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="h-4 w-4 text-gray-400">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
                 </svg>
 
-                <svg v-if="!lifeEvent.collapsed" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-gray-400">
+                <svg
+                  v-if="!lifeEvent.collapsed"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="h-4 w-4 text-gray-400">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
                 </svg>
               </div>
 
               <!-- date of life event -->
               <div v-if="!lifeEvent.collapsed" class="flex items-center border-b border-gray-200 px-3 py-2 text-sm">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-gray-500 mr-1">
-                  <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke-width="1.5"
+                  stroke="currentColor"
+                  class="mr-1 h-4 w-4 text-gray-500">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
 
                 {{ lifeEvent.happened_at }}
               </div>
 
               <!-- participants -->
-              <div v-if="!lifeEvent.collapsed" class="p-3 pb-1 flex">
+              <div v-if="!lifeEvent.collapsed" class="flex p-3 pb-1">
                 <div v-for="contact in lifeEvent.participants" :key="contact.id" class="mr-4">
                   <contact-card :contact="contact" :avatarClasses="'h-5 w-5 rounded-full mr-2'" :displayName="true" />
                 </div>
@@ -155,10 +222,10 @@ const toggleLifeEventVisibility = (lifeEvent) => {
 
             <!-- add a new life event to the timeline -->
             <div class="ml-6 mb-2">
-              <span @click="showAddLifeEventModalForTimelineEventId = timelineEvent.id"
+              <span
+                @click="showAddLifeEventModalForTimelineEventId = timelineEvent.id"
                 v-if="showAddLifeEventModalForTimelineEventId != timelineEvent.id"
-                class="text-sm text-blue-500 hover:underline cursor-pointer"
-                >
+                class="cursor-pointer text-sm text-blue-500 hover:underline">
                 Add another life event
               </span>
 
@@ -172,7 +239,6 @@ const toggleLifeEventVisibility = (lifeEvent) => {
                 @life-event-created="refreshLifeEvents" />
             </div>
           </div>
-
         </div>
       </div>
 
