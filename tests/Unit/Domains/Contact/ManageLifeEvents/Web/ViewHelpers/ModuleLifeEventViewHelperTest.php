3145193ca9cec2b291597c1d7f309a56bb3f31e0
Regis Freyd <djaiss@users.noreply.github.com>
2023-02-01 20:42:41 -0500

    Update ModuleLifeEventViewHelperTest.php

--- tests/Unit/Domains/Contact/ManageLifeEvents/Web/ViewHelpers/ModuleLifeEventViewHelperTest.php
+++ tests/Unit/Domains/Contact/ManageLifeEvents/Web/ViewHelpers/ModuleLifeEventViewHelperTest.php
@@ -50,7 +50,7 @@ class ModuleLifeEventViewHelperTest extends TestCase
         $this->assertEquals(
             [
                 'lifeEvents' => env('APP_URL').'/vaults/'.$contact->vault->id.'/contacts/'.$contact->id.'/timelineEvents',
-                'store' => env('APP_URL').'/vaults/'.$contact->vault->id.'/contacts/'.$contact->id.'/timelineEvents',
+                'load' => env('APP_URL').'/vaults/'.$contact->vault->id.'/contacts/'.$contact->id.'/timelineEvents',
             ],
             $array['url']
         );
