18b56d3d4c196bc14d149cf4d02caebab69a2cca
Regis Freyd <djaiss@users.noreply.github.com>
2023-02-09 08:19:56 -0500

    fixes

--- app/Domains/Contact/ManageLifeEvents/Web/Controllers/ContactModuleTimelineEventController.php
+++ app/Domains/Contact/ManageLifeEvents/Web/Controllers/ContactModuleTimelineEventController.php
@@ -84,35 +84,6 @@ class ContactModuleTimelineEventController extends Controller
         ], 201);
     }
 
-    public function update(Request $request, int $vaultId, int $contactId, int $loanId)
-    {
-        $loaners = collect($request->input('loaners'))->pluck('id');
-        $loanees = collect($request->input('loanees'))->pluck('id');
-
-        $data = [
-            'account_id' => Auth::user()->account_id,
-            'author_id' => Auth::id(),
-            'vault_id' => $vaultId,
-            'contact_id' => $contactId,
-            'loan_id' => $loanId,
-            'currency_id' => $request->input('currency_id'),
-            'type' => $request->input('type'),
-            'name' => $request->input('name'),
-            'description' => $request->input('description'),
-            'loaner_ids' => $loaners,
-            'loanee_ids' => $loanees,
-            'amount_lent' => $request->input('amount_lent') ? $request->input('amount_lent') * 100 : null,
-            'loaned_at' => $request->input('loaned_at'),
-        ];
-
-        $loan = (new UpdateLoan())->execute($data);
-        $contact = Contact::find($contactId);
-
-        return response()->json([
-            'data' => ModuleLoanViewHelper::dtoLoan($loan, $contact, Auth::user()),
-        ], 200);
-    }
-
     public function destroy(Request $request, int $vaultId, int $contactId, int $timelineEventId)
     {
         $data = [
