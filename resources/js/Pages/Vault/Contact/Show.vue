5308f3bd98529a03543638e923c3b064a3877230
Regis Freyd <djaiss@users.noreply.github.com>
2023-02-12 13:25:27 -0500

    wip

--- resources/js/Pages/Vault/Contact/Show.vue
+++ resources/js/Pages/Vault/Contact/Show.vue
@@ -145,6 +145,10 @@ const destroyAvatar = () => {
 .group-list-item:not(:last-child):after {
   content: ',';
 }
+
+.icon-sidebar {
+  color: #646b76;
+}
 </style>
 
 <template>
@@ -268,6 +272,47 @@ const destroyAvatar = () => {
 
           <!-- right -->
           <div class="p-3 sm:px-3 sm:py-0">
+            <!-- quick facts -->
+            <div class="p-3 mb-8 rounded-lg border border-gray-200 dark:border-gray-700 shadow-sm bg-gray-50 dark:bg-gray-800">
+              <div class="flex items-center justify-between">
+                <div class="flex items-center mr-1">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="icon-sidebar relative inline h-4 w-4 mr-1">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
+                  </svg>
+
+                  <p class="text-sm font-bold">Quick facts</p>
+                </div>
+
+                <!-- chevrons -->
+                <div>
+                  <svg
+                    @click="toggleLifeEventVisibility(lifeEvent)"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="mr-2 h-4 w-4 text-gray-400">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                  </svg>
+
+                  <!-- <svg
+                    @click="toggleLifeEventVisibility(lifeEvent)"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="mr-2 h-4 w-4 text-gray-400">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+                  </svg> -->
+                </div>
+              </div>
+              <div>
+                Food preferencies
+              </div>
+            </div>
+
             <!-- family summary -->
             <div v-if="data.group_summary_information.length > 0">
               <div class="mb-6 flex rounded border border-gray-200 p-3 dark:border-gray-700">
