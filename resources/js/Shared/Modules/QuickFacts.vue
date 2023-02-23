f2bc646ed9d3c3b24021e2845f6e60857b697bcc
Regis Freyd <djaiss@users.noreply.github.com>
2023-02-22 21:47:05 -0500

    fxes

--- resources/js/Shared/Modules/QuickFacts.vue
+++ resources/js/Shared/Modules/QuickFacts.vue
@@ -59,12 +59,10 @@ const get = (template) => {
   loading.value = true;
   localTemplate.value = template;
 
-  axios
-    .get(template.url.show)
-    .then((response) => {
-      localQuickFacts.value = response.data.data.quick_facts;
-      loading.value = false;
-    });
+  axios.get(template.url.show).then((response) => {
+    localQuickFacts.value = response.data.data.quick_facts;
+    loading.value = false;
+  });
 };
 
 const store = () => {
@@ -98,21 +96,28 @@ const update = (quickFact) => {
 };
 
 const destroy = (quickFact) => {
-  axios
-    .delete(quickFact.url.destroy)
-    .then(() => {
-      var id = localQuickFacts.value.findIndex((x) => x.id === quickFact.id);
-      localQuickFacts.value.splice(id, 1);
-    });
+  axios.delete(quickFact.url.destroy).then(() => {
+    var id = localQuickFacts.value.findIndex((x) => x.id === quickFact.id);
+    localQuickFacts.value.splice(id, 1);
+  });
 };
 </script>
 
 <template>
-  <div class="p-3 mb-8 rounded-lg border border-gray-200 dark:border-gray-700 shadow-sm bg-gray-50 dark:bg-gray-800">
-    <div @click="toggle()" class="flex items-center justify-between cursor-pointer" :class="openState ? ' mb-4' : ''">
-      <div class="flex items-center mr-1">
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="relative inline h-4 w-4 mr-1">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
+  <div class="mb-8 rounded-lg border border-gray-200 bg-gray-50 p-3 shadow-sm dark:border-gray-700 dark:bg-gray-800">
+    <div @click="toggle()" class="flex cursor-pointer items-center justify-between" :class="openState ? ' mb-4' : ''">
+      <div class="mr-1 flex items-center">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="relative mr-1 inline h-4 w-4">
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
         </svg>
 
         <p class="text-sm font-bold">Quick facts</p>
@@ -127,7 +132,7 @@ const destroy = (quickFact) => {
           viewBox="0 0 24 24"
           stroke-width="1.5"
           stroke="currentColor"
-          class="mr-2 h-4 w-4 text-gray-400 cursor-pointer">
+          class="mr-2 h-4 w-4 cursor-pointer text-gray-400">
           <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
         </svg>
 
@@ -138,7 +143,7 @@ const destroy = (quickFact) => {
           viewBox="0 0 24 24"
           stroke-width="1.5"
           stroke="currentColor"
-          class="mr-2 h-4 w-4 text-gray-400 cursor-pointer">
+          class="mr-2 h-4 w-4 cursor-pointer text-gray-400">
           <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
         </svg>
       </div>
@@ -146,33 +151,49 @@ const destroy = (quickFact) => {
 
     <div v-if="openState">
       <!-- templates -->
-      <div class="flex mb-4">
+      <div class="mb-4 flex">
         <ul class="list">
-          <li v-for="template in data.templates" :key="template.id" class="inline mr-2">
-            <span @click="get(template)" :class="localTemplate.id === template.id ? 'border border-gray-200 rounded bg-white font-semibold' : ''"  class="px-2 py-1 text-sm cursor-pointer">{{ template.label }}</span>
+          <li v-for="template in data.templates" :key="template.id" class="mr-2 inline">
+            <span
+              @click="get(template)"
+              :class="localTemplate.id === template.id ? 'rounded border border-gray-200 bg-white font-semibold' : ''"
+              class="cursor-pointer px-2 py-1 text-sm"
+              >{{ template.label }}</span
+            >
           </li>
         </ul>
       </div>
 
       <!-- content -->
       <ul v-if="localQuickFacts.length > 0 && !loading">
-        <li v-for="quickFact in localQuickFacts" :key="quickFact.id" class="border-b border-gray-300 border-dotted px-2 py-2 hover:bg-gray-100">
-
+        <li
+          v-for="quickFact in localQuickFacts"
+          :key="quickFact.id"
+          class="border-b border-dotted border-gray-300 px-2 py-2 hover:bg-gray-100">
           <!-- normal mode -->
-          <div v-if="editedQuickFactId !== quickFact.id" class="flex justify-between items-center">
+          <div v-if="editedQuickFactId !== quickFact.id" class="flex items-center justify-between">
             <div class="flex items-center">
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 text-green-600 mr-1 flex-none">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="mr-1 h-4 w-4 flex-none text-green-600">
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
               </svg>
 
               <span class="grow">{{ quickFact.content }}</span>
             </div>
 
             <hover-menu
-                  :show-edit="true"
-                  :show-delete="true"
-                  @edit="showEditQuickFactModal(quickFact)"
-                  @delete="destroy(quickFact)" />
+              :show-edit="true"
+              :show-delete="true"
+              @edit="showEditQuickFactModal(quickFact)"
+              @delete="destroy(quickFact)" />
           </div>
 
           <!-- edit mode -->
@@ -202,17 +223,26 @@ const destroy = (quickFact) => {
             </div>
           </form>
         </li>
-        <li v-if="!createQuickFactModalShown" class="ml-2 mt-1"><span @click="showCreateQuickFactModal()" class="cursor-pointer text-blue-500 hover:underline">+ add another</span></li>
+        <li v-if="!createQuickFactModalShown" class="ml-2 mt-1">
+          <span @click="showCreateQuickFactModal()" class="cursor-pointer text-blue-500 hover:underline"
+            >+ add another</span
+          >
+        </li>
       </ul>
 
       <!-- blank state -->
       <div v-if="localQuickFacts.length == 0 && !loading && !createQuickFactModalShown" class="mb-2">
-        <p class="px-5 pb-2 text-center">There are no quick facts here yet. <span @click="showCreateQuickFactModal" class="text-blue-500 hover:underline cursor-pointer">Add one now</span></p>
+        <p class="px-5 pb-2 text-center">
+          There are no quick facts here yet.
+          <span @click="showCreateQuickFactModal" class="cursor-pointer text-blue-500 hover:underline"
+            >Add one now</span
+          >
+        </p>
         <img src="/img/contact_blank_quick_facts.svg" :alt="$t('Activity feed')" class="mx-auto h-20 w-20" />
       </div>
 
       <!-- loading mode -->
-      <div v-if="loading"  class="px-20 py-5 text-center">
+      <div v-if="loading" class="px-20 py-5 text-center">
         <Loading />
       </div>
 
@@ -242,7 +272,6 @@ const destroy = (quickFact) => {
           <pretty-button :text="'Save'" :state="loadingState" :icon="'plus'" :classes="'save'" />
         </div>
       </form>
-
     </div>
   </div>
 </template>
