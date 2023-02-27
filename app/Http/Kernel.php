e867ed372e1b141d0b8ba88ce5778f3a6e4095d1
Alexis Saettler <alexis@saettler.org>
2023-02-27 10:58:00 +0100

    fix

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -47,7 +47,6 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \App\Http\Middleware\HandleInertiaRequests::class,
-            \App\Http\Middleware\CheckLocale::class,
         ],
 
         'api' => [
