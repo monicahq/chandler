8c6c604f9a6e09c9d54f00729253c8732d625c11
Regis Freyd <djaiss@users.noreply.github.com>
2023-03-05 11:07:46 -0500

    wip

--- app/Helpers/DistanceHelper.php
+++ app/Helpers/DistanceHelper.php
@@ -8,17 +8,12 @@ class DistanceHelper
 {
     /**
      * Format the distance according to the preferences of the user.
-     *
-     * @param  User  $user
-     * @param  int  $distance
-     * @param  string  $unit
-     * @return string
      */
     public static function format(User $user, int $distance, string $unit): string
     {
         if ($user->distance_format === User::DISTANCE_UNIT_KM && $unit === User::DISTANCE_UNIT_MILES) {
             $distance = round($distance * 1.609344, 2);
-        } else if ($user->distance_format === User::DISTANCE_UNIT_MILES && $unit === User::DISTANCE_UNIT_KM) {
+        } elseif ($user->distance_format === User::DISTANCE_UNIT_MILES && $unit === User::DISTANCE_UNIT_KM) {
             $distance = round($distance / 1.609344, 2);
         }
 
