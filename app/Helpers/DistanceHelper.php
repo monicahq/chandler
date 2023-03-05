027df9cba0fcd186a63c16e8ea575cfc6901cfb0
Mazarin <djaiss@users.noreply.github.com>
2023-03-05 11:06:41 -0500

    Update app/Helpers/DistanceHelper.php
    Co-authored-by: Alexis Saettler <alexis@saettler.org>

--- app/Helpers/DistanceHelper.php
+++ app/Helpers/DistanceHelper.php
@@ -16,16 +16,10 @@ class DistanceHelper
      */
     public static function format(User $user, int $distance, string $unit): string
     {
-        if ($user->distance_format === User::DISTANCE_UNIT_KM) {
-            if ($unit === User::DISTANCE_UNIT_MILES) {
-                $distance = round($distance * 1.609344, 2);
-            }
-        }
-
-        if ($user->distance_format === User::DISTANCE_UNIT_MILES) {
-            if ($unit === User::DISTANCE_UNIT_KM) {
-                $distance = round($distance / 1.609344, 2);
-            }
+        if ($user->distance_format === User::DISTANCE_UNIT_KM && $unit === User::DISTANCE_UNIT_MILES) {
+            $distance = round($distance * 1.609344, 2);
+        } else if ($user->distance_format === User::DISTANCE_UNIT_MILES && $unit === User::DISTANCE_UNIT_KM) {
+            $distance = round($distance / 1.609344, 2);
         }
 
         return trans('app.distance_format_'.$user->distance_format, ['count' => $distance]);
