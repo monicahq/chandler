06209a150e04659f5e435abaec69fae000a720fa
Alexis Saettler <alexis@saettler.org>
2023-04-08 23:21:00 +0200

    optimize

--- app/Domains/Contact/ManageContact/Web/Controllers/ContactVCardController.php
+++ app/Domains/Contact/ManageContact/Web/Controllers/ContactVCardController.php
@@ -6,34 +6,34 @@ use App\Domains\Contact\Dav\Services\ExportVCard;
 use App\Http\Controllers\Controller;
 use App\Models\Contact;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ContactVCardController extends Controller
 {
     public function download(Request $request, int $vault_id, int $contact_id)
     {
-        $carddata = $this->exportVCard($request, $vault_id, $contact_id);
+        $carddata = $this->exportVCard($vault_id, $contact_id);
 
         $contact = Contact::findOrFail($contact_id);
 
-        return response($carddata)
-            ->withHeaders([
-                'Content-Type' => 'text/vcard',
-                'Content-Disposition' => 'attachment; filename="'.$contact->uuid.'.vcf"',
-            ]);
+        return response()->streamDownload(function () use ($carddata) {
+            echo $carddata;
+        }, $contact->uuid.'.vcf', [
+            'Content-Type' => 'text/vcard',
+        ], 'inline');
     }
 
     /**
      * Get the new exported version of the object.
      */
-    protected function exportVCard(Request $request, int $vault_id, int $contact_id): string
+    protected function exportVCard(int $vault_id, int $contact_id): string
     {
-        $vcard = app(ExportVCard::class)
-            ->execute([
-                'account_id' => $request->user()->account_id,
-                'author_id' => $request->user()->id,
-                'vault_id' => $vault_id,
-                'contact_id' => $contact_id,
-            ]);
+        $vcard = app(ExportVCard::class)->execute([
+            'account_id' => Auth::user()->account_id,
+            'author_id' => Auth::id(),
+            'vault_id' => $vault_id,
+            'contact_id' => $contact_id,
+        ]);
 
         return $vcard->serialize();
     }
